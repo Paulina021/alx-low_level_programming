f3a3e06982f4b9c0dcf04ce9ea3b8ea18fe8fbd6
Paulina021 <paulinamakhubele@outlook.com>
2023-06-17 01:47:25 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -  The starting point of the program`*
+ * Return: Always 0 indicating success
+ */
+int main(void)
+}
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
