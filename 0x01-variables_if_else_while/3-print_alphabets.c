e55e68d39ff9d7c71c443b24d0d89eeefcd3c8d0
Paulina021 <paulinamakhubele@outlook.com>
2023-06-17 17:30:35 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Starting point of the program
+ * Return: always 0 to indicate success
+ */
+int main(void)
+{
+char c;
+c = 'a';
+while
+(c <= 'z');
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
