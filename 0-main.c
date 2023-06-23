b2ac722af5505feec9c6a152973e5cdc32460e92
Paulina021 <paulinamakhubele@outlook.com>
2023-06-23 14:34:30 -0700

    0-main.c

---
+++ 0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - Test the function for positive or negative
+ * Return: 0 successful executio0x03-debugging
+ */
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
