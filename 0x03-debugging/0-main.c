a8ee30d1240c1a60ca1aaaa2a2082557733b37e0
Paulina021 <paulinamakhubele@outlook.com>
2023-06-22 17:14:09 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - tests function that prints if integeris positive or negative
+ * Return: 0
+ */
+int main(vpid)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
