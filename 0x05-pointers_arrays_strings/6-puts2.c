4ccacf9b4dace6dbbd7e8141da7635a7d38c3147
Paulina021 <paulinamakhubele@outlook.com>
2023-06-26 19:01:52 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,16 +6,12 @@
  */
 void puts2(char *str)
 {
-int i = 0;
-for (; str[i] != '0' ; i++)
+int i;
+for (i = 0 ; str[i] != '\0' ; i++)
 {
 if (i % 2 == 0)
 {
 _putchar(str[i]);
-}
-else
-{
-	continue;
 }
 }
 }
